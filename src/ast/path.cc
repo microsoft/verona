d434b51e1172ed4c9891ad9e933502e66142b9ad
Sylvan Clebsch <syclebsc@microsoft.com>
2020-05-19 16:19:11 +0100

    add win32 path implementations

--- src/ast/path.cc
+++ src/ast/path.cc
@@ -13,6 +13,10 @@ namespace path
     constexpr auto link = "/proc/self/exe";
     char buffer[PATH_MAX];
     return realpath(link, buffer);
+#elif defined(_WIN32)
+    char p[MAX_PATH];
+    GetModuleFileName(NULL, p, MAX_PATH);
+    return std::string(p);
 #else
 #  error "path::executable not supported on this target."
 #endif
@@ -20,15 +24,21 @@ namespace path
 
   std::string directory(const std::string& path)
   {
+    constexpr auto delim =
 #if defined(__linux__)
-    size_t pos = path.rfind("/");
+      "/"
+#elif defined(_WIN32)
+      "\\"
+#else
+#  error "path::directory not supported on this target."
+#endif
+      ;
+
+    size_t pos = path.rfind(delim);
 
     if ((pos == std::string::npos) || (pos >= (path.size() - 1)))
       return path;
 
     return path.substr(0, pos);
-#else
-#  error "path::directory not supported on this target."
-#endif
   }
 }
