b8a0f914679fd92d4bda08fe8530fd97ada4293a
Matthew Parkinson <mattpark@microsoft.com>
2020-06-04 11:56:16 +0100

    Add example for #84

---
+++ src/rt/test/func/weak_leak_bug/weak_leak_bug.cc
@@ -0,0 +1,38 @@
+// Copyright (c) Project Verona contributers. All rights reserved.
+// Licensed under the MIT License.
+
+// See issue #84 for origin of this test.
+#include <test/harness.h>
+
+struct MyCown : VCown<MyCown>
+{};
+
+struct Msg : VAction<Msg>
+{
+  MyCown* m;
+  Msg(MyCown* m) : m(m) {}
+
+  void f()
+  {
+    Systematic::cout() << "Msg on " << m << std::endl;
+  }
+};
+
+void run_test()
+{
+  MyCown* t = new MyCown;
+  // HERE: the weak RC is never released.
+  t->weak_acquire();
+
+  Cown::schedule<Msg, YesTransfer>(t, t);
+}
+
+int main(int argc, char** argv)
+{
+  SystematicTestHarness h(argc, argv);
+
+  Scheduler::set_detect_leaks(false);
+  h.detect_leaks = false;
+
+  h.run(run_test);
+}
