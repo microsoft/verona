93cc788b5d4879e83158c26f4ea55746fb8c5693
Paul Liétar <paul@lietar.net>
2020-07-22 01:46:14 +0100

    Add a basic type hierarchy
    Only integers, capabilities, unions and intersections are supported.

---
+++ src/mlir/dialect/VeronaTypes.cc
@@ -0,0 +1,320 @@
+#include "dialect/VeronaTypes.h"
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/StandardTypes.h"
+
+namespace mlir::verona::detail
+{
+  struct MeetTypeStorage : public TypeStorage
+  {
+    using KeyTy = llvm::ArrayRef<Type>;
+
+    llvm::ArrayRef<Type> elements;
+
+    MeetTypeStorage(llvm::ArrayRef<Type> elements) : elements(elements) {}
+
+    bool operator==(const KeyTy& key) const
+    {
+      return key == elements;
+    }
+
+    static MeetTypeStorage*
+    construct(TypeStorageAllocator& allocator, const KeyTy& key)
+    {
+      llvm::ArrayRef<mlir::Type> elements = allocator.copyInto(key);
+      return new (allocator.allocate<MeetTypeStorage>())
+        MeetTypeStorage(elements);
+    }
+  };
+
+  struct JoinTypeStorage : public TypeStorage
+  {
+    using KeyTy = llvm::ArrayRef<Type>;
+
+    llvm::ArrayRef<Type> elements;
+
+    JoinTypeStorage(llvm::ArrayRef<Type> elements) : elements(elements) {}
+
+    bool operator==(const KeyTy& key) const
+    {
+      return key == elements;
+    }
+
+    static JoinTypeStorage*
+    construct(TypeStorageAllocator& allocator, const KeyTy& key)
+    {
+      llvm::ArrayRef<mlir::Type> elements = allocator.copyInto(key);
+      return new (allocator.allocate<JoinTypeStorage>())
+        JoinTypeStorage(elements);
+    }
+  };
+
+  struct IntegerTypeStorage : public ::mlir::TypeStorage
+  {
+    uint8_t width;
+    enum SignType
+    {
+      Unknown,
+      Unsigned,
+      Signed
+    };
+    unsigned sign;
+
+    // width, sign
+    using KeyTy = std::tuple<size_t, unsigned>;
+    IntegerTypeStorage(const KeyTy& key)
+    : width(std::get<0>(key)), sign(std::get<1>(key))
+    {}
+
+    bool operator==(const KeyTy& key) const
+    {
+      return key == KeyTy(width, sign);
+    }
+
+    static IntegerTypeStorage*
+    construct(TypeStorageAllocator& allocator, const KeyTy& key)
+    {
+      return new (allocator.allocate<IntegerTypeStorage>())
+        IntegerTypeStorage(key);
+    }
+  };
+
+  struct CapabilityTypeStorage : public ::mlir::TypeStorage
+  {
+    Capability capability;
+
+    using KeyTy = Capability;
+
+    CapabilityTypeStorage(const KeyTy& key) : capability(key) {}
+
+    static llvm::hash_code hashKey(const KeyTy& key)
+    {
+      return llvm::hash_value(key);
+    }
+
+    bool operator==(const KeyTy& key) const
+    {
+      return key == capability;
+    }
+
+    static CapabilityTypeStorage*
+    construct(TypeStorageAllocator& allocator, const KeyTy& key)
+    {
+      return new (allocator.allocate<CapabilityTypeStorage>())
+        CapabilityTypeStorage(key);
+    }
+  };
+} // namespace mlir::verona::detail
+
+namespace mlir::verona
+{
+  MeetType MeetType::get(MLIRContext* ctx, llvm::ArrayRef<mlir::Type> elements)
+  {
+    return Base::get(ctx, VeronaTypes::Meet, elements);
+  }
+
+  llvm::ArrayRef<mlir::Type> MeetType::getElements() const
+  {
+    return getImpl()->elements;
+  }
+
+  JoinType JoinType::get(MLIRContext* ctx, llvm::ArrayRef<mlir::Type> elements)
+  {
+    return Base::get(ctx, VeronaTypes::Join, elements);
+  }
+
+  llvm::ArrayRef<mlir::Type> JoinType::getElements() const
+  {
+    return getImpl()->elements;
+  }
+
+  IntegerType IntegerType::get(MLIRContext* ctx, size_t width, unsigned sign)
+  {
+    return Base::get(ctx, VeronaTypes::Integer, width, sign);
+  }
+
+  size_t IntegerType::getWidth() const
+  {
+    return getImpl()->width;
+  }
+
+  bool IntegerType::getSign() const
+  {
+    return getImpl()->sign;
+  }
+
+  CapabilityType CapabilityType::get(MLIRContext* ctx, Capability cap)
+  {
+    return Base::get(ctx, VeronaTypes::Capability, cap);
+  }
+
+  Capability CapabilityType::getCapability() const
+  {
+    return getImpl()->capability;
+  }
+
+  static ParseResult
+  parseTypeList(DialectAsmParser& parser, llvm::SmallVectorImpl<Type>& result)
+  {
+    if (parser.parseLess())
+      return failure();
+
+    if (succeeded(parser.parseOptionalGreater()))
+      return success();
+
+    do
+    {
+      mlir::Type element = parseVeronaType(parser);
+      if (!element)
+        return failure();
+
+      result.push_back(element);
+    } while (succeeded(parser.parseOptionalComma()));
+
+    if (parser.parseGreater())
+      return failure();
+
+    return success();
+  }
+
+  static Type parseMeetType(MLIRContext* ctx, DialectAsmParser& parser)
+  {
+    SmallVector<mlir::Type, 2> elements;
+    if (parseTypeList(parser, elements))
+      return Type();
+    return MeetType::get(ctx, elements);
+  }
+
+  static Type parseJoinType(MLIRContext* ctx, DialectAsmParser& parser)
+  {
+    SmallVector<mlir::Type, 2> elements;
+    if (parseTypeList(parser, elements))
+      return Type();
+    return JoinType::get(ctx, elements);
+  }
+
+  static Type parseIntegerType(
+    MLIRContext* ctx, DialectAsmParser& parser, StringRef keyword)
+  {
+    size_t width = 0;
+    if (keyword.substr(1).getAsInteger(10, width))
+    {
+      parser.emitError(parser.getNameLoc(), "unknown verona type: ") << keyword;
+      return Type();
+    }
+    bool sign = keyword.startswith("S");
+    return IntegerType::get(ctx, width, sign);
+  }
+
+  Type parseVeronaType(DialectAsmParser& parser)
+  {
+    MLIRContext* ctx = parser.getBuilder().getContext();
+
+    StringRef keyword;
+    if (parser.parseKeyword(&keyword))
+      return Type();
+
+    if (keyword == "meet")
+      return parseMeetType(ctx, parser);
+    else if (keyword == "join")
+      return parseJoinType(ctx, parser);
+    else if (keyword == "top")
+      return MeetType::get(ctx, {});
+    else if (keyword == "bottom")
+      return JoinType::get(ctx, {});
+    else if (keyword == "iso")
+      return CapabilityType::get(ctx, Capability::Isolated);
+    else if (keyword == "mut")
+      return CapabilityType::get(ctx, Capability::Mutable);
+    else if (keyword.startswith("U") || keyword.startswith("S"))
+      return parseIntegerType(ctx, parser, keyword);
+
+    parser.emitError(parser.getNameLoc(), "unknown verona type: ") << keyword;
+    return Type();
+  }
+
+  static void printTypeList(ArrayRef<Type> types, DialectAsmPrinter& os)
+  {
+    os << "<";
+    llvm::interleaveComma(
+      types, os, [&](auto element) {
+        // Verona types are closed
+        assert(isa_verona_type(element));
+        printVeronaType(element, os);
+      });
+    os << ">";
+  }
+
+  void printVeronaType(Type type, DialectAsmPrinter& os)
+  {
+    switch (type.getKind())
+    {
+      case VeronaTypes::Integer:
+      {
+        auto iTy = type.cast<IntegerType>();
+        if (iTy.getSign())
+        {
+          os << "S";
+        }
+        else
+        {
+          os << "U";
+        }
+        os << iTy.getWidth();
+        break;
+      }
+
+      case VeronaTypes::Meet:
+      {
+        auto meetType = type.cast<MeetType>();
+        if (meetType.getElements().empty())
+        {
+          os << "top";
+        }
+        else
+        {
+          os << "meet";
+          printTypeList(meetType.getElements(), os);
+        }
+        break;
+      }
+
+      case VeronaTypes::Join:
+      {
+        auto joinType = type.cast<JoinType>();
+        if (joinType.getElements().empty())
+        {
+          os << "bottom";
+        }
+        else
+        {
+          os << "join";
+          printTypeList(joinType.getElements(), os);
+        }
+        break;
+      }
+
+      case VeronaTypes::Capability:
+      {
+        auto capType = type.cast<CapabilityType>();
+        switch (capType.getCapability())
+        {
+          case Capability::Isolated:
+            os << "iso";
+            break;
+          case Capability::Mutable:
+            os << "mut";
+            break;
+        }
+        break;
+      }
+    }
+  }
+
+  bool isa_verona_type(Type type)
+  {
+    return type.getKind() >= FIRST_VERONA_TYPE &&
+      type.getKind() < LAST_VERONA_TYPE;
+  }
+}
---
+++ src/mlir/dialect/VeronaTypes.h
@@ -0,0 +1,104 @@
+// Copyright Microsoft and Project Verona Contributors.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "mlir/IR/Dialect.h"
+
+namespace mlir::verona
+{
+  Type
+  parseVeronaType(DialectAsmParser& parser);
+  void printVeronaType(Type type, DialectAsmPrinter& os);
+
+  bool isa_verona_type(Type type);
+
+  namespace detail
+  {
+    struct MeetTypeStorage;
+    struct JoinTypeStorage;
+    struct IntegerTypeStorage;
+    struct CapabilityTypeStorage;
+  }
+
+  // In the long term we should claim a range in LLVM's DialectSymbolRegistry,
+  // rather than use the "experimental" range.
+  static constexpr unsigned FIRST_VERONA_TYPE =
+    Type::Kind::FIRST_PRIVATE_EXPERIMENTAL_0_TYPE;
+  static constexpr unsigned LAST_VERONA_TYPE =
+    Type::Kind::LAST_PRIVATE_EXPERIMENTAL_0_TYPE;
+
+  namespace VeronaTypes
+  {
+    enum Kind
+    {
+      Meet = FIRST_VERONA_TYPE,
+      Join,
+      Integer,
+      Capability,
+    };
+  }
+
+  struct MeetType
+  : public Type::TypeBase<MeetType, Type, detail::MeetTypeStorage>
+  {
+    using Base::Base;
+    static MeetType
+    get(MLIRContext* ctx, llvm::ArrayRef<mlir::Type> elementTypes);
+    llvm::ArrayRef<mlir::Type> getElements() const;
+
+    static bool kindof(unsigned kind)
+    {
+      return kind == VeronaTypes::Meet;
+    }
+  };
+
+  struct JoinType
+  : public Type::TypeBase<JoinType, Type, detail::JoinTypeStorage>
+  {
+    using Base::Base;
+    static JoinType
+    get(MLIRContext* ctx, llvm::ArrayRef<mlir::Type> elementTypes);
+    llvm::ArrayRef<mlir::Type> getElements() const;
+
+    static bool kindof(unsigned kind)
+    {
+      return kind == VeronaTypes::Join;
+    }
+  };
+
+  struct IntegerType
+  : public Type::TypeBase<IntegerType, Type, detail::IntegerTypeStorage>
+  {
+    using Base::Base;
+
+    static IntegerType get(MLIRContext* context, size_t width, unsigned sign);
+
+    size_t getWidth() const;
+    bool getSign() const;
+
+    static bool kindof(unsigned kind)
+    {
+      return kind == VeronaTypes::Integer;
+    }
+  };
+
+  enum class Capability
+  {
+    Isolated,
+    Mutable,
+  };
+
+  struct CapabilityType
+  : public Type::TypeBase<CapabilityType, Type, detail::CapabilityTypeStorage>
+  {
+    using Base::Base;
+    static CapabilityType get(MLIRContext* ctx, Capability cap);
+    Capability getCapability() const;
+
+    static bool kindof(unsigned kind)
+    {
+      return kind == VeronaTypes::Capability;
+    }
+  };
+}
