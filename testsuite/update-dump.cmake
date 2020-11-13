88fc230b1af6f0e634c40f5fc91f49869ac55d50
Paul Liétar <plietar@users.noreply.github.com>
2020-11-13 12:15:59 +0000

    Fix and tidy the testsuite CMake scripts. (#347)
    - Don't pass the -g option to `verona-mlir`. That binary doesn't support it.
    - Correct the path for the IR output file in update-dump. It should be
      `mlir.txt`, not `out.mlir`.
    - Use the `CheckStatus` helper in `ast-parse.cmake` and `mlir-parse.cmake`.
      This is equivalent to `execute_process`, but has builtin support for
      checking the exit code.
    - Change the name of the PARSER and VERONAM variables to match the
      actual binary names. This is neater as we add more binaries.

--- testsuite/update-dump.cmake
+++ testsuite/update-dump.cmake
@@ -12,18 +12,17 @@ foreach(TEST_FOLDER ${TEST_FOLDERS})
       set(OUT_FILE ${TEST_FOLDER}/ast-parse/${TEST_NAME}/ast.txt)
       message(STATUS "Regenerating ${OUT_FILE}")
       execute_process(
-        COMMAND ${PARSER} -a -g ${GRAMMAR} ${PARSE_TEST}
+        COMMAND ${VERONA_AST} -a -g ${GRAMMAR} ${PARSE_TEST}
         OUTPUT_FILE ${OUT_FILE})
     endforeach()
   elseif(${TEST_FOLDER} MATCHES ".*/mlir")
     file(GLOB MLIR_TESTS "${TEST_FOLDER}/mlir-parse/*.verona" "${TEST_FOLDER}/mlir-parse/*.mlir")
     foreach(MLIR_TEST ${MLIR_TESTS})
       get_filename_component(TEST_NAME ${MLIR_TEST} NAME_WE)
-      set(OUT_FILE ${TEST_FOLDER}/mlir-parse/${TEST_NAME}/out.mlir)
+      set(OUT_FILE ${TEST_FOLDER}/mlir-parse/${TEST_NAME}/mlir.txt)
       message(STATUS "Regenerating ${OUT_FILE}")
       execute_process(
-        COMMAND ${MLIRGEN} -g ${GRAMMAR} ${PARSE_TEST} -o -
-        OUTPUT_FILE ${OUT_FILE})
+        COMMAND ${VERONA_MLIR} ${MLIR_TEST} -o ${OUT_FILE})
     endforeach()
   else()
     execute_process(COMMAND ${PYTHON_EXECUTABLE}
