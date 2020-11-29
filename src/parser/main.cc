f8fb0bb401b8650f83b60c5b945b5d7cd4774878
Sylvan Clebsch <sylvan.clebsch@microsoft.com>
2020-11-29 10:10:04 +0000

    ast printing

--- src/parser/main.cc
+++ src/parser/main.cc
@@ -1,9 +1,12 @@
 // Copyright Microsoft and Project Verona Contributors.
 // SPDX-License-Identifier: MIT
 #include "parser.h"
+#include "print.h"
 
 int main(int argc, char** argv)
 {
+  using namespace verona::parser;
+
   err::Errors err;
 
   // auto opt = cli::parse(argc, argv);
@@ -13,6 +16,9 @@ int main(int argc, char** argv)
 
   // if (opt.ast)
   //   std::cout << m;
+
+  auto ast = parse("filename", err);
+  std::cout << pretty(ast);
 
   std::cerr << err;
   return err.empty() ? 0 : -1;
