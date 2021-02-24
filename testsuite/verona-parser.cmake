8b8d994c0f41c7e48b9e25dbbc80e5c2b0ba4810
Matthew Parkinson <mattpark@microsoft.com>
2021-02-24 11:40:25 +0000

    Add flag to Verona Parser tests.

--- testsuite/verona-parser.cmake
+++ testsuite/verona-parser.cmake
@@ -1,6 +1,6 @@
-# Arguments for testing Veronac
+# Arguments for testing verona-parser
 macro(toolargs ARGS testfile outputdir)
-  set(${ARGS} ${testfile} --ast)
+  set(${ARGS} ${testfile} --ast --validate)
 endmacro()
 
 # Set extension for verona tests
