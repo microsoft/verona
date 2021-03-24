7e5b248833ee3e8cb0fa93cd191683f13d5f7fbd
Sylvan Clebsch <sylvan.clebsch@microsoft.com>
2021-03-24 03:45:42 +0000

    rewrite lambda patterns as parameters with a  clause

--- testsuite/verona-parser.cmake
+++ testsuite/verona-parser.cmake
@@ -1,6 +1,6 @@
 # Arguments for testing verona-parser
 macro(toolargs ARGS testfile outputdir)
-  set(${ARGS} ${testfile} --ast --validate)
+  set(${ARGS} ${testfile} --ast --validate --anf)
 endmacro()
 
 # Set extension for verona tests
