9fb171fa13f6e74a4b28cd56139b907572086a3c
Istvan Haller <ihaller@microsoft.com>
2021-04-09 11:52:34 +0100

    Reverted changed to harness.h

--- src/rt/test/harness.h
+++ src/rt/test/harness.h
@@ -51,17 +51,17 @@ public:
 
     if (seed_upper < seed_lower)
     {
-      Systematic::cout() << "Seed_upper " << seed_upper << " seed_lower "
-                         << seed_lower << Systematic::endl;
+      std::cout << "Seed_upper " << seed_upper << " seed_lower " << seed_lower
+                << std::endl;
       abort();
     }
 
     cores = opt.is<size_t>("--cores", 4);
-    Systematic::cout() << " --cores " << cores << Systematic::endl;
+    std::cout << " --cores " << cores << std::endl;
 
     detect_leaks = !opt.has("--allow_leaks");
     if (!detect_leaks)
-      Systematic::cout() << " --allow_leaks " << Systematic::endl;
+      std::cout << " --allow_leaks " << std::endl;
     Scheduler::set_detect_leaks(detect_leaks);
 
 #if defined(_WIN32) && defined(CI_BUILD)
@@ -84,7 +84,7 @@ public:
 #endif
     for (seed = seed_lower + random; seed < seed_upper + random; seed++)
     {
-      Systematic::cout() << "Seed: " << seed << Systematic::endl;
+      std::cout << "Seed: " << seed << std::endl;
 
       Scheduler& sched = Scheduler::get();
 #ifdef USE_SYSTEMATIC_TESTING
@@ -108,9 +108,9 @@ public:
       if (detect_leaks)
         snmalloc::current_alloc_pool()->debug_check_empty();
       high_resolution_clock::time_point t1 = high_resolution_clock::now();
-      Systematic::cout() << "Time so far: "
-                         << duration_cast<seconds>((t1 - start)).count()
-                         << " seconds" << Systematic::endl;
+      std::cout << "Time so far: "
+                << duration_cast<seconds>((t1 - start)).count() << " seconds"
+                << std::endl;
     }
   }
 
