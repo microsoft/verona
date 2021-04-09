91cf45281ec946f3c010cc416b4487ff48d052c7
Renato Golin <rengolin@systemcall.eu>
2021-04-09 17:04:05 +0100

    Parse limited type, retTy, literals

--- src/mlir/generator.cc
+++ src/mlir/generator.cc
@@ -8,10 +8,19 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/Types.h"
 
+#include <string>
+
 using namespace verona::parser;
 
 namespace
 {
+  /// Helper to make sure the basic block has a terminator
+  bool hasTerminator(mlir::Block* bb)
+  {
+    return !bb->getOperations().empty() && bb->back().isKnownTerminator();
+  }
+
+  /// Get node as a shared pointer of a sub-type
   template<class T>
   Node<T> nodeAs(Ast from)
   {
@@ -97,7 +106,7 @@ namespace mlir::verona
           break;
         case Kind::Function:
         {
-          auto func = parseFunction(sub);
+          auto func = parseNode(sub);
           if (auto err = func.takeError())
             return std::move(err);
           scope.push_back(func->get<FuncOp>());
@@ -118,6 +127,17 @@ namespace mlir::verona
     {
       case Kind::Class:
         return parseClass(ast);
+      case Kind::Function:
+        return parseFunction(ast);
+      case Kind::Lambda:
+        return parseLambda(ast);
+      case Kind::Character:
+      case Kind::Int:
+      case Kind::Float:
+      case Kind::Hex:
+      case Kind::Binary:
+      case Kind::Bool:
+        return parseLiteral(ast);
       default:
         // TODO: Implement all others
         break;
@@ -136,7 +156,13 @@ namespace mlir::verona
     // TODO: Lower arguments
     llvm::SmallVector<llvm::StringRef, 1> argNames;
     Types types;
-    llvm::SmallVector<mlir::Type, 1> retTy;
+
+    // Check return type (TODO: implement multiple returns)
+    Types retTy;
+    if (func->result)
+    {
+      retTy.push_back(parseType(func->result));
+    }
 
     // Declare all arguments on current scope
     SymbolScopeT var_scope(symbolTable);
@@ -147,34 +173,145 @@ namespace mlir::verona
       return std::move(err);
     auto& funcIR = *def;
 
-    // TODO: Lower body
+    // Lower body
+    auto body = func->body;
+    auto last = parseNode(body);
+    if (auto err = last.takeError())
+      return std::move(err);
 
-    // TODO: Lower return value
-    builder.create<mlir::ReturnOp>(loc);
+    // Check if needs to return a value at all
+    if (hasTerminator(builder.getBlock()))
+      return funcIR;
+
+    // Lower return value
+    // (TODO: cast type if not the same)
+    bool hasLast = last->hasValue();
+
+    if (hasLast)
+    {
+      auto retVal = last->get<Value>();
+      builder.create<ReturnOp>(loc, retVal);
+    }
+    else
+    {
+      builder.create<ReturnOp>(loc);
+    }
 
     return funcIR;
   }
 
+  llvm::Expected<ReturnValue> Generator::parseLambda(Ast ast)
+  {
+    auto lambda = nodeAs<Lambda>(ast);
+    assert(lambda && "Bad Node");
+
+    // Blocks add lexical context
+    SymbolScopeT var_scope{symbolTable};
+
+    ReturnValue last;
+    llvm::SmallVector<Ast, 1> nodes;
+    for (auto sub : lambda->body)
+    {
+      auto node = parseNode(sub);
+      if (auto err = node.takeError())
+        return std::move(err);
+      last = *node;
+    }
+    return last;
+  }
+
+  llvm::Expected<ReturnValue> Generator::parseLiteral(Ast ast)
+  {
+    auto loc = getLocation(ast);
+    switch (ast->kind())
+    {
+      case Kind::Int:
+      {
+        auto I = nodeAs<Int>(ast);
+        assert(I && "Bad Node");
+        auto str = I->location.view();
+        auto val = std::stol(str.data());
+        auto type = parseType(ast);
+        auto op = builder.create<ConstantIntOp>(loc, val, type);
+        return op->getOpResult(0);
+        break;
+      }
+      case Kind::Character:
+      case Kind::Float:
+      case Kind::Hex:
+      case Kind::Binary:
+      case Kind::Bool:
+        assert(false && "Not implemented yet");
+      default:
+        assert(false && "Bad Node");
+    }
+
+    return Value();
+  }
+
+  Type Generator::parseType(Ast ast)
+  {
+    switch (ast->kind())
+    {
+      case Kind::Int:
+        // TODO: Understand what the actual size is
+        return builder.getIntegerType(64);
+      case Kind::Float:
+        // TODO: Understand what the actual size is
+        return builder.getF64Type();
+      case Kind::TypeRef:
+      {
+        auto R = nodeAs<TypeRef>(ast);
+        assert(R && "Bad Node");
+        // TODO: Implement type list
+        return parseType(R->typenames[0]);
+      }
+      case Kind::TypeName:
+      {
+        auto C = nodeAs<TypeName>(ast);
+        assert(C && "Bad Node");
+        auto name = C->location.view();
+        // TODO: differentiate between signed and unsigned
+        Type type = llvm::StringSwitch<Type>(name)
+                      .Case("U32", builder.getI32Type())
+                      .Case("U64", builder.getI64Type())
+                      .Case("F32", builder.getF32Type())
+                      .Case("F64", builder.getF64Type())
+                      .Default(Type());
+        assert(type && "Classes not implemented yet");
+        return type;
+      }
+      case Kind::Character:
+      case Kind::Hex:
+      case Kind::Binary:
+      case Kind::Bool:
+        assert(false && "Not implemented yet");
+      default:
+        assert(false && "Bad Node");
+    }
+    return Type();
+  }
+
   // ===================================================== MLIR Generators
-  llvm::Expected<mlir::FuncOp> Generator::generateProto(
-    mlir::Location loc,
+  llvm::Expected<FuncOp> Generator::generateProto(
+    Location loc,
     llvm::StringRef name,
-    llvm::ArrayRef<mlir::Type> types,
-    llvm::ArrayRef<mlir::Type> retTy)
+    llvm::ArrayRef<Type> types,
+    llvm::ArrayRef<Type> retTy)
   {
     // Create function
-    auto funcTy = builder.getFunctionType(types, retTy);
-    auto func = mlir::FuncOp::create(loc, name, funcTy);
-    func.setVisibility(mlir::SymbolTable::Visibility::Private);
+    auto funcTy = builder.getFunctionType(types, {retTy});
+    auto func = FuncOp::create(loc, name, funcTy);
+    func.setVisibility(SymbolTable::Visibility::Private);
     return functionTable.insert(name, func);
   }
 
-  llvm::Expected<mlir::FuncOp> Generator::generateEmptyFunction(
-    mlir::Location loc,
+  llvm::Expected<FuncOp> Generator::generateEmptyFunction(
+    Location loc,
     llvm::StringRef name,
     llvm::ArrayRef<llvm::StringRef> args,
-    llvm::ArrayRef<mlir::Type> types,
-    llvm::ArrayRef<mlir::Type> retTy)
+    llvm::ArrayRef<Type> types,
+    llvm::ArrayRef<Type> retTy)
   {
     assert(args.size() == types.size() && "Argument/type mismatch");
 
