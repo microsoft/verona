6cc62cd2e3a746561ee185d0d4a4aabb9ef8d2c9
Renato Golin <rengolin@systemcall.eu>
2021-04-16 13:23:34 +0100

    Remove old MLIR generator

--- src/mlir/old/dialect/TypecheckInterface.cc
+++
@@ -1,6 +0,0 @@
-// Copyright Microsoft and Project Verona Contributors.
-// SPDX-License-Identifier: MIT
-
-#include "old/dialect/TypecheckInterface.h"
-
-#include "old/dialect/TypecheckInterface.cpp.inc"
--- src/mlir/old/dialect/TypecheckInterface.h
+++
@@ -1,9 +0,0 @@
-// Copyright Microsoft and Project Verona Contributors.
-// // SPDX-License-Identifier: MIT
-
-#pragma once
-#include "mlir/IR/OpDefinition.h"
-
-// This line must come after "normal" includes.
-// Having this comment prevents clang-format from re-ordering it.
-#include "old/dialect/TypecheckInterface.h.inc"
