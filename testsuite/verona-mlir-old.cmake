bcc74364685a6ce9b122d6a61773651e2631e2a9
Renato Golin <rengolin@systemcall.eu>
2021-04-16 13:23:34 +0100

    Remove old MLIR generator

--- testsuite/verona-mlir-old.cmake
+++
@@ -1,8 +0,0 @@
-# Arguments for testing verona-mlir
-macro(toolargs ARGS testfile outputdir)
-set(${ARGS} ${testfile} -o ${outputdir}/mlir.txt)
-endmacro()
-
-# Set extension for verona-mlir tests
-set(TEST_EXTENSION verona mlir)
-
