722eecc43c80ed12c04458a79a6a66daec366890
Marios Kogias <marioskogias@gmail.com>
2021-05-25 10:36:43 +0100

    Add LambdaBehaviours (#439)
    * Create a behaviour from a C++ lambda
    
    * Add func test for lambdabehaviour
    
    * Add smart pointer test for lambda function

---
+++ src/rt/cpp/lambdabehaviour.h
@@ -0,0 +1,72 @@
+// Copyright Microsoft and Project Verona Contributors.
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "vbehaviour.h"
+#include "vobject.h"
+
+namespace verona::rt
+{
+  class EmptyCown : public VCown<EmptyCown>
+  {
+  public:
+    EmptyCown() {}
+  };
+
+  template<class T>
+  class LambdaBehaviour : public Behaviour
+  {
+    friend class Cown;
+
+  private:
+    T fn;
+
+    static void f(Behaviour* msg)
+    {
+      auto t = static_cast<LambdaBehaviour<T>*>(msg);
+      t->fn();
+
+      if constexpr (!std::is_trivially_destructible_v<LambdaBehaviour<T>>)
+      {
+        t->~LambdaBehaviour<T>();
+      }
+    }
+
+    static const Behaviour::Descriptor* desc()
+    {
+      static constexpr Behaviour::Descriptor desc = {
+        sizeof(LambdaBehaviour<T>), f, NULL};
+
+      return &desc;
+    }
+
+    void* operator new(size_t, LambdaBehaviour* obj)
+    {
+      return obj;
+    }
+
+    void operator delete(void*, LambdaBehaviour*) {}
+
+    void* operator new(size_t) = delete;
+    void* operator new[](size_t size) = delete;
+    void operator delete[](void* p) = delete;
+    void operator delete[](void* p, size_t sz) = delete;
+
+  public:
+    LambdaBehaviour(T fn_) : Behaviour(desc()), fn(std::move(fn_)) {}
+  };
+
+  template<typename T>
+  static void scheduleLambda(Cown* c, T f)
+  {
+    Cown::schedule<LambdaBehaviour<T>>(c, std::forward<T>(f));
+  }
+
+  template<typename T>
+  static void scheduleLambda(T f)
+  {
+    Cown* c = new EmptyCown();
+    Cown::schedule<LambdaBehaviour<T>>(c, std::forward<T>(f));
+    Cown::release(ThreadAlloc::get(), c);
+  }
+} // namespace verona::rt
