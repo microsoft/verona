aa287af88eba734a6415bc816c19f3495bee9fe3
Renato Golin <rengolin@systemcall.eu>
2021-05-25 12:45:20 +0100

    Merge utils into generator/consumer
    The use of the helper functions was disjoint by either and both
    consumers and transformers will need to include the generator, so it
    makes sense for all MLIR utils to be in the generator and the AST utils
    to be in the consumer only.
    
    Renaming generator functions to reduce redundancy when calling it from
    the generator object, for example:
      From: generator.generateAutoCast(...)
        To:       gen.AutoCast(...)

--- src/mlir/generator.cc
+++ src/mlir/generator.cc
@@ -3,23 +3,166 @@
 
 #include "generator.h"
 
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/Types.h"
-#include "utils.h"
 
 #include <string>
 
-/// LLVM aliases
-using StructType = mlir::LLVM::LLVMStructType;
-using PointerType = mlir::LLVM::LLVMPointerType;
-using ArrayType = mlir::LLVM::LLVMArrayType;
-
 namespace mlir::verona
 {
-  Value MLIRGenerator::typeConversion(Value val, Type ty)
+  // ====== Helpers to interface consumers and transformers with the generator
+
+  OpBuilder& MLIRGenerator::getBuilder()
+  {
+    return builder;
+  }
+
+  SymbolTableT& MLIRGenerator::getSymbolTable()
+  {
+    return symbolTable;
+  }
+
+  void MLIRGenerator::push_back(FuncOp func)
+  {
+    module->push_back(func);
+  }
+
+  OwningModuleRef MLIRGenerator::finish()
+  {
+    return std::move(module);
+  }
+
+  // ==================================== Generic helpers that manipulate MLIR
+
+  bool MLIRGenerator::hasTerminator(mlir::Block* bb)
+  {
+    return !bb->getOperations().empty() &&
+      bb->back().mightHaveTrait<mlir::OpTrait::IsTerminator>();
+  }
+
+  bool MLIRGenerator::isPointer(mlir::Value val)
+  {
+    return val && val.getType().isa<PointerType>();
+  }
+
+  mlir::Type MLIRGenerator::getElementType(mlir::Value val)
+  {
+    assert(isPointer(val) && "Bad type");
+    return val.getType().dyn_cast<PointerType>().getElementType();
+  }
+
+  bool MLIRGenerator::isStructPointer(mlir::Value val)
+  {
+    return isPointer(val) && getElementType(val).isa<StructType>();
+  }
+
+  mlir::Type MLIRGenerator::getFieldType(StructType type, int offset)
+  {
+    auto field = type.getBody().begin();
+    std::advance(field, offset);
+    return PointerType::get(*field);
+  }
+
+  // ==================================================== Top level generators
+
+  llvm::Expected<FuncOp> MLIRGenerator::Proto(
+    Location loc,
+    llvm::StringRef name,
+    llvm::ArrayRef<Type> types,
+    llvm::ArrayRef<Type> retTy)
+  {
+    // Create function
+    auto funcTy = builder.getFunctionType(types, {retTy});
+    auto func = FuncOp::create(loc, name, funcTy);
+    // FIXME: This should be private unless we export, but for now we make
+    // it public to test IR generation before implementing public visibility
+    func.setVisibility(SymbolTable::Visibility::Public);
+    return func;
+  }
+
+  llvm::Expected<FuncOp> MLIRGenerator::EmptyFunction(
+    Location loc,
+    llvm::StringRef name,
+    llvm::ArrayRef<Type> types,
+    llvm::ArrayRef<Type> retTy)
+  {
+    // If it's not declared yet, do so. This simplifies direct declaration
+    // of compiler functions. User functions should be checked at the parse
+    // level.
+    auto func = module->lookupSymbol<FuncOp>(name);
+    if (!func)
+    {
+      auto proto = Proto(loc, name, types, retTy);
+      if (auto err = proto.takeError())
+        return std::move(err);
+      func = *proto;
+    }
+
+    // Create entry block, set builder entry point
+    auto& entryBlock = *func.addEntryBlock();
+    builder.setInsertionPointToStart(&entryBlock);
+
+    return func;
+  }
+
+  llvm::Expected<Value> MLIRGenerator::Call(
+    Location loc, FuncOp func, llvm::ArrayRef<Value> args)
+  {
+    // TODO: Implement dynamic method calls
+    auto call = builder.create<CallOp>(loc, func, args);
+    // TODO: Implement multiple return values (tuples?)
+    return call->getOpResult(0);
+  }
+
+  llvm::Expected<Value> MLIRGenerator::Arithmetic(
+    Location loc, llvm::StringRef opName, Value lhs, Value rhs)
+  {
+    // FIXME: Implement all unary and binary operators
+    assert(lhs && rhs && "No binary operation with less than two arguments");
+
+    // Make sure we're dealing with values, not pointers
+    // FIXME: This shouldn't be necessary at this point
+    if (isPointer(lhs))
+      lhs = Load(loc, lhs);
+    if (isPointer(rhs))
+      rhs = Load(loc, rhs);
+
+    // Promote types to be the same, or ops don't work, in the end, both
+    // types are identical and the same as the return type.
+    std::tie(lhs, rhs) = Promote(lhs, rhs);
+    auto retTy = lhs.getType();
+
+    // FIXME: We already converted U32 to i32 so this "works". But we need
+    // to make sure we want that conversion as early as it is, and if not,
+    // we need to implement this as a standard select and convert that
+    // later. However, that would only work if U32 has a method named "+",
+    // or if we declare it on the fly and then clean up when we remove the
+    // call.
+
+    // Floating point arithmetic
+    if (retTy.isF32() || retTy.isF64())
+    {
+      auto op = llvm::StringSwitch<Value>(opName)
+                  .Case("+", builder.create<AddFOp>(loc, retTy, lhs, rhs))
+                  .Default({});
+      assert(op && "Unknown arithmetic operator");
+      return op;
+    }
+
+    // Integer arithmetic
+    assert(retTy.isa<IntegerType>() && "Bad arithmetic types");
+    auto op = llvm::StringSwitch<Value>(opName)
+                .Case("+", builder.create<AddIOp>(loc, retTy, lhs, rhs))
+                .Default({});
+    assert(op && "Unknown arithmetic operator");
+    return op;
+  }
+
+  // ==================================================== Low level generators
+
+  Value MLIRGenerator::Convert(Value val, Type ty)
   {
     auto valTy = val.getType();
     auto valSize = valTy.getIntOrFloatBitWidth();
@@ -58,7 +201,7 @@ namespace mlir::verona
   }
 
   std::pair<mlir::Value, mlir::Value>
-  MLIRGenerator::typePromotion(mlir::Value lhs, mlir::Value rhs)
+  MLIRGenerator::Promote(mlir::Value lhs, mlir::Value rhs)
   {
     auto lhsType = lhs.getType();
     auto rhsType = rhs.getType();
@@ -72,125 +215,32 @@ namespace mlir::verona
 
     // Promote the smallest to the largest
     if (lhsSize < rhsSize)
-      lhs = typeConversion(lhs, rhsType);
+      lhs = Convert(lhs, rhsType);
     else
-      rhs = typeConversion(rhs, lhsType);
+      rhs = Convert(rhs, lhsType);
 
     return {lhs, rhs};
   }
 
-  llvm::Expected<FuncOp> MLIRGenerator::generateProto(
-    Location loc,
-    llvm::StringRef name,
-    llvm::ArrayRef<Type> types,
-    llvm::ArrayRef<Type> retTy)
-  {
-    // Create function
-    auto funcTy = builder.getFunctionType(types, {retTy});
-    auto func = FuncOp::create(loc, name, funcTy);
-    // FIXME: This should be private unless we export, but for now we make
-    // it public to test IR generation before implementing public visibility
-    func.setVisibility(SymbolTable::Visibility::Public);
-    return func;
-  }
-
-  llvm::Expected<FuncOp> MLIRGenerator::generateEmptyFunction(
-    Location loc,
-    llvm::StringRef name,
-    llvm::ArrayRef<Type> types,
-    llvm::ArrayRef<Type> retTy)
-  {
-    // If it's not declared yet, do so. This simplifies direct declaration
-    // of compiler functions. User functions should be checked at the parse
-    // level.
-    auto func = module->lookupSymbol<FuncOp>(name);
-    if (!func)
-    {
-      auto proto = generateProto(loc, name, types, retTy);
-      if (auto err = proto.takeError())
-        return std::move(err);
-      func = *proto;
-    }
-
-    // Create entry block, set builder entry point
-    auto& entryBlock = *func.addEntryBlock();
-    builder.setInsertionPointToStart(&entryBlock);
-
-    return func;
-  }
-
-  llvm::Expected<Value> MLIRGenerator::generateCall(
-    Location loc, FuncOp func, llvm::ArrayRef<Value> args)
-  {
-    // TODO: Implement dynamic method calls
-    auto call = builder.create<CallOp>(loc, func, args);
-    // TODO: Implement multiple return values (tuples?)
-    return call->getOpResult(0);
-  }
-
-  llvm::Expected<Value> MLIRGenerator::generateArithmetic(
-    Location loc, llvm::StringRef opName, Value lhs, Value rhs)
-  {
-    // FIXME: Implement all unary and binary operators
-    assert(lhs && rhs && "No binary operation with less than two arguments");
-
-    // Make sure we're dealing with values, not pointers
-    // FIXME: This shouldn't be necessary at this point
-    if (isPointer(lhs))
-      lhs = generateLoad(loc, lhs);
-    if (isPointer(rhs))
-      rhs = generateLoad(loc, rhs);
-
-    // Promote types to be the same, or ops don't work, in the end, both
-    // types are identical and the same as the return type.
-    std::tie(lhs, rhs) = typePromotion(lhs, rhs);
-    auto retTy = lhs.getType();
-
-    // FIXME: We already converted U32 to i32 so this "works". But we need
-    // to make sure we want that conversion as early as it is, and if not,
-    // we need to implement this as a standard select and convert that
-    // later. However, that would only work if U32 has a method named "+",
-    // or if we declare it on the fly and then clean up when we remove the
-    // call.
-
-    // Floating point arithmetic
-    if (retTy.isF32() || retTy.isF64())
-    {
-      auto op = llvm::StringSwitch<Value>(opName)
-                  .Case("+", builder.create<AddFOp>(loc, retTy, lhs, rhs))
-                  .Default({});
-      assert(op && "Unknown arithmetic operator");
-      return op;
-    }
-
-    // Integer arithmetic
-    assert(retTy.isa<IntegerType>() && "Bad arithmetic types");
-    auto op = llvm::StringSwitch<Value>(opName)
-                .Case("+", builder.create<AddIOp>(loc, retTy, lhs, rhs))
-                .Default({});
-    assert(op && "Unknown arithmetic operator");
-    return op;
-  }
-
-  Value MLIRGenerator::generateAlloca(Location loc, Type ty)
+  Value MLIRGenerator::Alloca(Location loc, Type ty)
   {
     PointerType pointerTy;
-    Value len = generateConstant(builder.getI32Type(), 1);
+    Value len = Constant(builder.getI32Type(), 1);
     pointerTy = PointerType::get(ty);
     return builder.create<LLVM::AllocaOp>(loc, pointerTy, len);
   }
 
-  Value MLIRGenerator::generateGEP(Location loc, Value addr, int offset)
+  Value MLIRGenerator::GEP(Location loc, Value addr, int offset)
   {
     llvm::SmallVector<Value> offsetList;
     // First argument is always in context of a list
     if (isStructPointer(addr))
     {
-      auto zero = generateZero(builder.getI32Type());
+      auto zero = Zero(builder.getI32Type());
       offsetList.push_back(zero);
     }
     // Second argument is in context of the struct
-    auto len = generateConstant(builder.getI32Type(), offset);
+    auto len = Constant(builder.getI32Type(), offset);
     offsetList.push_back(len);
     ValueRange index(offsetList);
     Type retTy = addr.getType();
@@ -199,17 +249,17 @@ namespace mlir::verona
     return builder.create<LLVM::GEPOp>(loc, retTy, addr, index);
   }
 
-  Value MLIRGenerator::generateLoad(Location loc, Value addr, int offset)
+  Value MLIRGenerator::Load(Location loc, Value addr, int offset)
   {
     if (!isa<LLVM::GEPOp>(addr.getDefiningOp()))
-      addr = generateGEP(loc, addr, offset);
+      addr = GEP(loc, addr, offset);
     else
       assert(offset == 0 && "Can't take an offset of a GEP");
     return builder.create<LLVM::LoadOp>(loc, addr);
   }
 
   Value
-  MLIRGenerator::generateAutoLoad(Location loc, Value addr, Type ty, int offset)
+  MLIRGenerator::AutoLoad(Location loc, Value addr, Type ty, int offset)
   {
     // If it's not an address, there's nothing to load
     if (!isPointer(addr))
@@ -225,14 +275,14 @@ namespace mlir::verona
     if (ty)
       assert(elmTy == ty && "Invalid pointer load");
 
-    return generateLoad(loc, addr, offset);
+    return Load(loc, addr, offset);
   }
 
   void
-  MLIRGenerator::generateStore(Location loc, Value addr, Value val, int offset)
+  MLIRGenerator::Store(Location loc, Value addr, Value val, int offset)
   {
     if (!isa<LLVM::GEPOp>(addr.getDefiningOp()))
-      addr = generateGEP(loc, addr, offset);
+      addr = GEP(loc, addr, offset);
     else
       assert(offset == 0 && "Can't take an offset of a GEP");
     builder.create<LLVM::StoreOp>(loc, val, addr);
@@ -264,7 +314,7 @@ namespace mlir::verona
     return name;
   }
 
-  Value MLIRGenerator::generateConstant(Type ty, std::variant<int, double> val)
+  Value MLIRGenerator::Constant(Type ty, std::variant<int, double> val)
   {
     // Use symbol table with mangled name to avoid duplication
     auto name = mangleConstantName(ty, val);
@@ -293,15 +343,15 @@ namespace mlir::verona
     return value;
   }
 
-  Value MLIRGenerator::generateZero(Type ty)
+  Value MLIRGenerator::Zero(Type ty)
   {
     if (ty.isa<FloatType>())
-      return generateConstant(ty, 0.0);
+      return Constant(ty, 0.0);
     else
-      return generateConstant(ty, 0);
+      return Constant(ty, 0);
   }
 
-  Value MLIRGenerator::generateConstantString(StringRef str, StringRef name)
+  Value MLIRGenerator::ConstantString(StringRef str, StringRef name)
   {
     // Use auto-generated name if none provided
     static size_t incr = 0;
