3300d3002d0256f4dc0185e2855b961596b525da
Renato Golin <rengolin@systemcall.eu>
2021-09-01 15:20:01 +0100

    unique as dtor

--- src/interop/CXXQuery.h
+++ src/interop/CXXQuery.h
@@ -201,15 +201,19 @@ namespace verona::interop
       // Search for class, enum or template.
       name = "::" + name;
       MatchFinder finder;
+      auto recDeclMatch = std::make_unique<CXXTypeMatch<clang::CXXRecordDecl>>(ty);
+      auto classTempMatch = std::make_unique<CXXTypeMatch<clang::ClassTemplateDecl>>(ty);
+      auto enumDeclMatch = std::make_unique<CXXTypeMatch<clang::EnumDecl>>(ty);
+
       finder.addMatcher(
         cxxRecordDecl(hasName(name)).bind("id"),
-        new CXXTypeMatch<clang::CXXRecordDecl>(ty));
+        recDeclMatch.get());
       finder.addMatcher(
         classTemplateDecl(hasName(name)).bind("id"),
-        new CXXTypeMatch<clang::ClassTemplateDecl>(ty));
+        classTempMatch.get());
       finder.addMatcher(
         enumDecl(hasName(name)).bind("id"),
-        new CXXTypeMatch<clang::EnumDecl>(ty));
+        enumDeclMatch.get());
       finder.matchAST(*ast);
 
       // Return the type matched directly.
