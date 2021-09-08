b05fcd62f667d45aa404ff17da0d1420b3fe2bec
David Chisnall <David.Chisnall@microsoft.com>
2021-09-08 17:21:43 +0100

    Fix using a fixed version and pick one to default to for now.

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -3,6 +3,9 @@
 # cache.
 if (NOT DEFINED FIXED_VCPKG_VERSION)
     set(FIXED_VCPKG_VERSION master)
+	set(FIXED_VCPKG_ZIP refs/heads/master)
+else()
+	set(FIXED_VCPKG_ZIP ${FIXED_VCPKG_VERSION})
 endif ()
 
 # Not used yet, but we may want to add custom triplets for ASAN and so on.
@@ -14,11 +17,17 @@ if (NOT DEFINED VCPKG_BOOTSTRAP_OPTIONS)
 endif()
 # Fetch vcpkg from GitHub
 if (NOT EXISTS ${VERONA_DEFAULT_VCPKG_LOCATION})
-    message(STATUS "Vcpkg not found, downloading from GitHub...")
-    file(DOWNLOAD https://github.com/microsoft/vcpkg/archive/refs/heads/${FIXED_VCPKG_VERSION}.zip vcpkg.zip)
-    file(ARCHIVE_EXTRACT INPUT vcpkg.zip)
+  set(DOWNLOAD_PATH "https://github.com/microsoft/vcpkg/archive/${FIXED_VCPKG_ZIP}.zip")
+  message(STATUS "Vcpkg not found, downloading from ${DOWNLOAD_PATH}")
+  file(DOWNLOAD ${DOWNLOAD_PATH} vcpkg.zip
+    SHOW_PROGRESS
+    TLS_VERIFY ON)
+  file(ARCHIVE_EXTRACT INPUT vcpkg.zip)
 endif()
 # Use vcpkg as the toolchain file.  This will bootstrap vcpkg.
 set(CMAKE_TOOLCHAIN_FILE ${VERONA_DEFAULT_VCPKG_LOCATION}
     CACHE STRING "Vcpkg toolchain file")
 
+if (NOT EXISTS ${VERONA_DEFAULT_VCPKG_LOCATION})
+	message(FATAL_ERROR "vcpkg toolchain file not found: ${VERONA_DEFAULT_VCPKG_LOCATION}")
+endif()
