034a5d8447a110ca8867214656a0c9a4af004aab
Marios Kogias <marioskogias@gmail.com>
2022-06-27 14:23:34 +0100

    wip: new api

--- src/rt/test/perf/smallbank/smallbank.cc
+++ src/rt/test/perf/smallbank/smallbank.cc
@@ -1,8 +1,35 @@
 #include <test/harness.h>
+#include <cpp/when.h>
+
+#include <unordered_map>
+
+using namespace verona::rt;
+
+struct Checking// : public VCown<Checking>
+{
+  uint64_t balance;
+};
+
+struct Savings// : public VCown<Savings>
+{
+  uint64_t balance;
+};
+
+struct Account
+{
+  cown_ptr<Checking> checking;
+  cown_ptr<Savings> savings;
+};
+
+void balance(std::unordered_map<std::string, Account *> &accounts, std::string user_id)
+{
+  auto *account = accounts.at(user_id);
+  when(account->checking, account->savings) << [](acquired_cown<Checking> ch, acquired_cown<Savings> sa) { std::cout << "hello with accounts\n"; };
+}
 
 void smallbank_body()
 {
-  schedule_lambda([](){ std::cout << "Hello smallbank\n"; });
+  when() << [](){ std::cout << "Hello smallbank\n"; };
 }
 
 int main(int argc, char** argv)
