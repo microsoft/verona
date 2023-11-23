3c962c671ddfa29932fef954e3141b843154427c
Matthew Parkinson <mattpark@microsoft.com>
2023-11-23 15:00:29 +0000

    Add dump of passes (removing stdlib)

--- testsuite/verona_nostdlib.cmake
+++ testsuite/verona_nostdlib.cmake
@@ -1,6 +1,6 @@
 # Arguments for testing verona-parser
 macro(toolinvoke ARGS local_dist testfile outputdir)
-  set(${ARGS} "${local_dist}/verona/verona${CMAKE_EXECUTABLE_SUFFIX}" build ${testfile} -w -o ${outputdir}/ast)
+  set(${ARGS} "${local_dist}/verona/verona${CMAKE_EXECUTABLE_SUFFIX}" build ${testfile} -w -o ${outputdir}/ast --dump_passes ${outputdir} --no-std -p defaultargs)
 endmacro()
 
 # Set extension for verona tests
