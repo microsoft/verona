d68a12d3f66761e1fe9c3abb76ed2d4ff3bbf2bd
Matthew Parkinson <mattpark@microsoft.com>
2024-04-23 15:58:22 +0100

    Clangformat

--- src/passes/defaultargs.cc
+++ src/passes/defaultargs.cc
@@ -44,11 +44,10 @@ namespace verona
               {
                 if (has_default)
                 {
-                  new_params << 
-                    err(
-                      param,
-                      "Can't put a parameter with no default value after a "
-                      "parameter with one");
+                  new_params << err(
+                    param,
+                    "Can't put a parameter with no default value after a "
+                    "parameter with one");
                   // Do not process further to prevent duplicate errors.
                   break;
                 }
